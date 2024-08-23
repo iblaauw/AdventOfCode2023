193f595f43154235f98a3bb0867dc2c90d4bb9e9
iblaauw <naibla8358@gmail.com>
2024-08-23 14:20:25 +0000

    Got day2 part 1 working!

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,7 @@
 mod game_input;
 
+use game_input::Game;
+
 fn main() {
     println!("Hello, world!");
     let helper = advent_utils::Helper::new(2);
@@ -8,8 +10,33 @@ fn main() {
     helper.print_solution(solution);
 }
 
-fn solve(_fh: advent_utils::FileHelper) -> u32 {
-    panic!("Not Implemented");
+fn solve(fh: advent_utils::FileHelper) -> u32 {
+
+    let mut result = 0;
+    for line in fh.into_lines() {
+        let game: Game = line.parse().unwrap();
+        if is_valid(&game) {
+            result += game.id;
+        }
+    }
+
+    result
+}
+
+fn is_valid(game: &Game) -> bool {
+    const MAX_RED: u32 = 12;
+    const MAX_GREEN: u32 = 13;
+    const MAX_BLUE: u32 = 14;
+
+    for d in &game.draws {
+        if (d.red > MAX_RED) ||
+            (d.green > MAX_GREEN) ||
+            (d.blue > MAX_BLUE) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 #[cfg(test)]
@@ -18,7 +45,7 @@ mod tests {
 
     #[test]
     fn test_given() {
-        let helper = advent_utils::Helper::new(1);
+        let helper = advent_utils::Helper::new(2);
         let fh = helper.open_file_with_name("given_test");
         let solution = solve(fh);
         assert_eq!(solution, 8);
