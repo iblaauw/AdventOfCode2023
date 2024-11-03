6b75665f2e75100f050239a91f3434be0b9214d7
iblaauw <naibla8358@gmail.com>
2024-11-03 19:03:55 +0000

    Implement mapping and add mapping tests

--- day5/src/location_map.rs
+++ day5/src/location_map.rs
@@ -20,12 +20,22 @@ impl LocationMap {
         }
     }
 
-    pub fn add_range(&mut self, line_range: &str) {
-        todo!()
+    pub fn add_range(&mut self, line_range: &str) -> Result<(), GenericError> {
+        let range: LocationMapRange = line_range.parse()?;
+        self.ranges.push(range);
+
+        Ok(())
     }
 
     pub fn map_location(&self, location: u32) -> u32 {
-        todo!()
+        for range in &self.ranges {
+            let new_loc = range.map_location(location);
+            if new_loc.is_some() {
+                return new_loc.unwrap();
+            }
+        }
+
+        return location;
     }
 }
 
@@ -41,5 +51,86 @@ impl FromStr for LocationMap {
         }
 
         Ok(LocationMap::new(splits[0], splits[2]))
+    }
+}
+
+impl LocationMapRange {
+    fn map_location(&self, value: u32) -> Option<u32> {
+        if value < self.source_start || value > (self.source_start + self.size) {
+            return None;
+        } else {
+            return Some(self.dest_start + (value - self.source_start));
+        }
+    }
+}
+
+impl FromStr for LocationMapRange {
+    type Err = GenericError;
+
+    fn from_str(line: &str) -> Result<Self, GenericError> {
+        let line = line.trim();
+        let values: Vec<u32> = parse_utils::parse_value_list(line, ' ')?;
+        if values.len() != 3 {
+            return GenericError::as_err(format!("Invalid location map range {}", line));
+        }
+
+        Ok(
+            LocationMapRange {
+                source_start: values[1],
+                dest_start: values[0],
+                size: values[2]
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating() {
+        let mut map: LocationMap = "alpha-to-omega map:".parse().expect("Failed to parse test");
+        map.add_range("5 12 42").expect("Failed to add first range");
+        map.add_range("8 21 37").expect("Failed to add second range");
+
+        assert_eq!(map.ranges.len(), 2);
+
+        let first_range = &map.ranges[0];
+        assert_eq!(first_range.source_start, 12);
+        assert_eq!(first_range.dest_start, 5);
+        assert_eq!(first_range.size, 42);
+
+        let second_range = &map.ranges[1];
+        assert_eq!(second_range.source_start, 21);
+        assert_eq!(second_range.dest_start, 8);
+        assert_eq!(second_range.size, 37);
+    }
+
+    #[test]
+    fn map_range() {
+        let map_range: LocationMapRange = "42 59 7".parse().expect("Failed to parse map");
+        
+        assert_eq!(map_range.map_location(3), None);
+        assert_eq!(map_range.map_location(59), Some(42));
+        assert_eq!(map_range.map_location(42), None);
+        assert_eq!(map_range.map_location(66), Some(49));
+        assert_eq!(map_range.map_location(67), None);
+        assert_eq!(map_range.map_location(58), None);
+        assert_eq!(map_range.map_location(63), Some(46));
+    }
+
+    #[test]
+    fn map_all() {
+        let mut map: LocationMap = "blob-to-square map:".parse().expect("Fail to parse map");
+        map.add_range("23 11 4").expect("Failed to add first range");
+        map.add_range("70 50 3").expect("Failed to add second range");
+        map.add_range("4 75 10").expect("Failed to add third range");
+
+        assert_eq!(map.map_location(42), 42);
+        assert_eq!(map.map_location(0), 0);
+        assert_eq!(map.map_location(10), 10);
+        assert_eq!(map.map_location(51), 71);
+        assert_eq!(map.map_location(80), 9);
+        assert_eq!(map.map_location(15), 27);
     }
 }
